1bbbc90d5b90c462befcacfe1040642c356ae8ad
Lucas Ultremare <65183236+LucasUltremare@users.noreply.github.com>
2022-02-16 21:44:49 -0300

    PW2
    First php

---
+++ teste01.php
@@ -0,0 +1,21 @@
+<html>
+
+<head></head>
+
+</form>
+<body>
+    
+    <?php 
+            echo "<h1>Registro de Aluno</h1>";
+            
+            $nome = "Lucas Ultremare";
+            $idade = "17";
+            
+
+            echo "Nome: $nome <br> Idade: $idade"
+
+            /*$hoje = date ('d/m/y h:i:s');
+            echo "A data de hj é $hoje";*/
+    ?>
+</body>
+</html>
