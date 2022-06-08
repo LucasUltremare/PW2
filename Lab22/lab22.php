9fa2f93484f60ba6418f84e5d80fa270990b0d17
Lucas Ultremare <65183236+LucasUltremare@users.noreply.github.com>
2022-06-08 20:47:28 -0300

    Fix votes

--- Lab22/lab22.php
+++ Lab22/lab22.php
@@ -1,51 +1,58 @@
 <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="">
+    <title>Lab22</title>
+</head>
+<body>
 
-<html>
+<?php
 
-    <head>
-        <title>Votos </title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    </head>
+$conn=pg_connect("host=localhost dbname=voto11 user=aluno password=3T3K3Q");
 
-    <body>
-        
-        <?php
 
-            $conn = pg_connect("host=localhost dbname=voto11 user=aluno password=3T3K3Q");
-            
-            if( !$conn ) {
-                die( "Erro de conexão com o banco de dados");
-            }
-        
-        ?>
 
-        <table border = '1'>
+$select=pg_query($conn, "SELECT * FROM eleicao");
 
-            <tr>
-                <th>Nomes</th>
-                <th>Votos</th>
-            </tr>
+   while ($pegarTabela = pg_fetch_assoc($select)){
 
-            <?php
 
-                $result = pg_query($conn, "SELECT * FROM eleicao order by votos desc");
 
-                    while ($row = pg_fetch_assoc($result)) {
+       echo "<table>";
 
-                        echo "<tr>";
-                        echo "<td> <a href='lab22.php?id=".$row['id']."'>".htmlspecialchars($row['nome'])."</a> </td>";
-                        echo "<td> <a href='lab22.php?id=".$row['id']."'>".htmlspecialchars($row['votos'])."</a> </td>";
-                        echo "</tr>";
+       echo "<tr>";
 
-                    }
+       echo "<td> <a href='lab22.php?id=".$pegarTabela['id']."'> ".$pegarTabela['nome']."|".$pegarTabela['votos']."</a></td>"; 
 
-                pg_close($conn);
+       echo "</tr>";
 
-            ?>
+       echo "</table>";
 
-        </table>
+   }
 
-    </body>
+
+?>
+
+<?php
+
+if (isset($_GET ['id'])) {
+
+
+
+    $id=$_GET['id'];
+
+    
+
+$update=pg_query($conn, "UPDATE eleicao SET votos=votos+1 WHERE id=$id");
+
+  
+
+}
+
+?>
+
+</body>
 </html>
